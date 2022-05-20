803f524c37b90f8ea33c5f3de980f52fb52a1942
lwabdallah945 <102518864+lwabdallah945@users.noreply.github.com>
2022-05-20 20:28:59 +0200

    Add files via upload

---
+++ difference.js
@@ -0,0 +1,10 @@
+function difference(){
+    var nbr1, nbr2, sum;
+    nbr1 = Number(document.getElementById("nbr1").value);
+    nbr2 = Number(document.getElementById("nbr2").value);
+    sum = nbr1 - nbr2;
+    (10.8).toFixed(2);
+    var num=sum;
+    alert("On vous rend "+ num.toFixed(2)+"€")
+    location.reload();
+}
